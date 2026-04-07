8f366e70bd170f462ae6e8758eb9fe02991cd589
Navneet Srivastava <oyenavneet@gmail.com>
2026-04-08 01:27:14 +0530

    aws bedrock

---
+++ sec01/bedrock/src/main/java/com/oyenavneet/openai/BedrockModelApplication.java
@@ -0,0 +1,13 @@
+package com.oyenavneet.openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedrockModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedrockModelApplication.class, args);
+	}
+
+}
