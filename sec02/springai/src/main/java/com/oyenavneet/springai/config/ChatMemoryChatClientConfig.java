8ac189fb45b0113ad23946ab93403fae191aacf7
Navneet Srivastava <oyenavneet@gmail.com>
2026-04-17 23:50:42 +0530

    chat memory stored  in db

--- sec02/springai/src/main/java/com/oyenavneet/springai/config/ChatMemoryChatClientConfig.java
+++ sec02/springai/src/main/java/com/oyenavneet/springai/config/ChatMemoryChatClientConfig.java
@@ -6,6 +6,8 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,16 @@ import java.util.List;
 
 @Configuration
 public class ChatMemoryChatClientConfig {
+
+    //Build custom Bean of Chat memory to configure own properties like maxMessage.
+    @Bean
+    ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        return MessageWindowChatMemory
+                .builder()
+                .maxMessages(10)
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .build();
+    }
 
     @Bean("chatMemoryChatClient")
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
